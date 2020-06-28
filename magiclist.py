ea7535af2e0625a83a5db2962456c306fa1afe05
Eli Plaut <eli@cogitosys.com>
2020-06-28 10:10:40 +0300

    #1: add cls_type for MagicList

--- magiclist.py
+++ magiclist.py
@@ -1,10 +1,24 @@
-from typing import Any
+from typing import Any, Type, Optional
 
 
 class MagicList(list):
+    def __init__(self, cls_type: Type = None, *args, **kwargs) -> None:
+        self._cls_type = cls_type  # type: Optional[cls_type]
+        super(MagicList, self).__init__(*args, **kwargs)
+
     def __setitem__(self, index: int, value: Any) -> None:
         if index == len(self):
             self.append(value)
         else:
             # this may raise IndexError for missing indexes
             super(MagicList, self).__setitem__(index, value)
+
+    def __getitem__(self, item: int) -> Any:
+        try:
+            return super(MagicList, self).__getitem__(item)
+        except IndexError:
+            if self._cls_type:
+                self[item] = self._cls_type()
+                return super(MagicList, self).__getitem__(item)
+            else:
+                raise
--- tests/test_mgiclist.py
+++ tests/test_mgiclist.py
@@ -9,6 +9,7 @@ class MagicListTest(unittest.TestCase):
         a[0] = 5
         a[1] = 42
         a[2] = 121
+        self.assertEqual(len(a), 3)
         self.assertEqual(a[0], 5)
         self.assertEqual(a[1], 42)
         self.assertEqual(a[2], 121)
@@ -22,3 +23,21 @@ class MagicListTest(unittest.TestCase):
         a = MagicList()
         with self.assertRaises(IndexError):
             b = a[1]
+
+
+class MagicListClsTypeTest(unittest.TestCase):
+    class Person:
+        def __init__(self, age: int = None) -> None:
+            self.age = age
+
+    def test_new_elem(self):
+        a = MagicList(cls_type=MagicListClsTypeTest.Person)
+        a[0].age = 5
+        self.assertEqual(len(a), 1)
+        self.assertIsInstance(a[0], MagicListClsTypeTest.Person)
+        self.assertEqual(a[0].age, 5)
+
+    def test_index_error_insertion(self):
+        a = MagicList()
+        with self.assertRaises(IndexError):
+            a[1].age = 42
